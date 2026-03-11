7434d2dcd32b26adc953e97b76c7318c1e9c1ff1
Juan Esteban Caicedo <juanes401890@gmail.com>
2026-03-11 17:35:47 +0100

    Para esperar a que el hijo acabe

--- fork1.c
+++ fork1.c
@@ -26,14 +26,40 @@ int main(){
     int main(){
     
         //Con la línea 3 y 4
-        
+        int n;
         pid_t pid1; -> A partir de aquí hay dos procesos ejecutándose. Estos se diferencian en el valor de la variable pid1. pid1==0 solo la cumple el hijo, esto ocurre siempre. El pid del padre siempre es mayor que 0, es por esto que definimos el if de la siguiente forma.
         pid1=fork();
         
         if(pid1==0){->dos procesos leen está linea, el padre no la ejecuta, pero el hijo si. 
             printf("Hola, soy el proceso hijo y tengo PID %d. El PID de mi padres es %d\n", getpid(), getppid());->petppid(get parent process identifier) 
+            scanf("%d", &n);
+            printf("Genial, has introducido el número %d\n", n);
         }else if(pid1 > 0){-> mientras que el padre ejecuta esta línea. 
             printf("Hola, soy el proceso padre con PID %d. Mi hijo tiene PID %d\n", getpid(), (int)pid1);
+        }else{
+            perror("ha ocurrido un error al crear el proceso");
+        }
+
+        return 0;
+    }
+    */
+
+    /*Para hacer que el padre espere:
+    int main(){
+    
+        //Con la línea 3 y 4
+        int n;
+        pid_t pid1; 
+        pid1=fork();
+        
+        if(pid1==0){
+            printf("Hola, soy el proceso hijo y tengo PID %d. El PID de mi padres es %d\n", getpid(), getppid());
+            scanf("%d", &n);
+            printf("Genial, has introducido el número %d\n", n);
+        }else if(pid1 > 0){
+            printf("Hola, soy el proceso padre con PID %d. Mi hijo tiene PID %d\n", getpid(), (int)pid1);
+            wait(NULL);->Hace que el padre espere al hijo.
+            puts("Ahora sí, el hijo ha acabadoy ya puedo morir en paz");
         }else{
             perror("ha ocurrido un error al crear el proceso");
         }
