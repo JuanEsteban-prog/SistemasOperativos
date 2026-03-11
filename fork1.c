aaaea2135a5356e926aa6d2cce41624aa2b58833
Juan Esteban Caicedo <juanes401890@gmail.com>
2026-03-11 17:17:07 +0100

    más sobre el tema

--- fork1.c
+++ fork1.c
@@ -3,10 +3,20 @@
 
 int main(){
 
-    puts("Esta línea sol la ejecuta el proceso padre");
+    puts("Esta línea solo la ejecuta el proceso padre");
 
     //A partir de aquí dos procesos se ejecutan!
     fork();
     puts("Esta línea la ejecuta el proceso padre y el hijo... pero quién es quién?");
     return 0;
+
+    /*Para saber que proceso hace que:
+    
+    printf("Esta línea la ejecuta el proceso padre, con PID: %d\n", getpid());
+
+    //A partir de aquí dos procesos se ejecutan!
+    fork();
+    puts("Esta línea la ejecuta el proceso hijo %d\n", getpid());
+    return 0;
+    */
 }
