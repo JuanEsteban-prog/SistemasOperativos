e0607416b74ae5b5576c665d4e041438f61ebca4
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2026-04-13 22:24:02 +0200

    Segundo ejercicio para. practicar

---
+++ Práctica Parcial/ejercicioDos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>     //-> Incluye printf() y scanf()
+#include <unistd.h>    // -> fork(), pipe(), read(), write(), close().
+#include <sys/types.h> //-> pid_t
+#include <sys/wait.h>  //-> wait(), waitpid()
+#include <stdlib.h>    //-> exit(), rand(), srand()
+#include <time.h>      //-> time()
+#include <string.h>    //-> strlen(), strcpy(), memcpy().
+
+int main()
+{
+
+    return 0:
+}
