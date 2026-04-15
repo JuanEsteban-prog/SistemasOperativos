cfc30404f5d59f6c5843dab7fc3c52c9615c3135
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2026-04-15 19:54:48 +0200

    Ejercicio en clase 15 de abril

--- 15_Abril/ejercicio.c
+++ 15_Abril/ejercicio.c
@@ -5,3 +5,64 @@ Se crea ambos procesos uno atras de otro. Una vez los dos estan creados, un prec
 Una vez que el proceso a leido el numero por teclado, el proceso escribe, accede a la memoria compartida y muestra el numero por pantalla.
 Ese mismo proceso escribe muestra un mensaje que dice: Pulsa una tecla para finalizar(Espera a que pulsemos una tecla para que muera).
 */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main()
+{
+
+    int *variable_compartida = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    int pipe1[2];
+    if (pipe(pipe1) == -1)
+    {
+        perror("Error al crear la tuberia");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t lee = fork();
+
+    if (lee < 0)
+    {
+        perror("Error al crear el proceso lee!");
+        exit(EXIT_FAILURE);
+    }
+    else if (lee == 0)
+    { // Proceso hijo (lee)
+        close(pipe1[0]);
+        int variableUsuario;
+        printf("Escribe un numero cualquiera en el teclado: \n");
+        scanf("%d", &variableUsuario);
+        *variable_compartida = variableUsuario;
+        write(pipe1[1], 1);
+    }
+    else if (lee > 0)
+    { // Proceso padre
+        pid_t escribe = fork();
+
+        if (escribe < 0)
+        {
+            perror("Error al crear el proceso escribe!");
+            exit(EXIT_FAILURE);
+        }
+        else if (escribe == 0)
+        { // Proceso hijo(escribe)
+            close(pipe1[1]);
+            printf("El numero es: %d", *variable_compartida);
+            exit(0);
+            // Espera que aplastemos una tecla para finalizar el proceso.
+        }
+        else if (escribe > 0)
+        { // Proceso padre.
+            wait(NULL);
+            wait(NULL);
+        }
+    }
+
+    munmap(variable_compartida, sizeof(int));
+    return 0;
+}
